f7072031525971f202c015f037b3fc6793510b1c
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2024-08-18 14:52:44 +0000

    event listener added to run getWeather when the page loads

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const apiKeyCode = "ecf9c35dccc24ecc834142452241808";
 const apiUrlExample = "http://api.weatherapi.com/v1/current.json?q=London&aqi=no";
 
+document.addEventListener("DOMContentLoaded", getWeather);
+
 async function getWeather() {
     const dataCheck = await fetch(apiUrlExample + `&key=${apiKeyCode}`);
     let response = await dataCheck.json();
