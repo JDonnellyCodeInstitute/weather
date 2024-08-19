858c7dda9ec6aff11ac5929b588fb22a2c49a899
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2024-08-19 14:51:52 +0000

    Main function created to update webapp based on API call. Event listener updated to include location search parameter

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,38 @@
 const apiKeyCode = "ecf9c35dccc24ecc834142452241808";
-const apiUrlExample = "https://api.weatherapi.com/v1/current.json?q=London&aqi=no";
+const apiUrlExample = `https://api.weatherapi.com/v1/current.json?key=${apiKeyCode}&aqi=no&q=`;
 
-document.addEventListener("DOMContentLoaded", getWeather);
+//Default at start
+document.addEventListener("DOMContentLoaded", function() {
+    displayWeather("Belfast");
+});
+/**
+ * Main function which uses the weather API call data to display
+ * location, wind speed, temperature and humidity.
+ */
+async function displayWeather(location) {
+    try {
+        const dataCheck = await fetch(apiUrlExample + location);
 
-async function getWeather() {
-    const dataCheck = await fetch(apiUrlExample + `&key=${apiKeyCode}`);
-    let response = await dataCheck.json();
+        if (!dataCheck.ok) {
+            throw new Error(`HTTP error! status: ${dataCheck.status}`);
+        }
 
-    console.log(response);
+        const response = await dataCheck.json();
+
+        // Logging the selected elements to ensure required elements are visible in DOM
+        const locationElement = document.querySelector('.location');
+        const temperatureElement = document.querySelector('.temperature');
+        const humidityElement = document.querySelector('.humidity');
+        const windElement = document.querySelector('.wind');
+        console.log(locationElement, temperatureElement, humidityElement, windElement);
+
+        document.querySelector('.location').innerHTML = response.location.name;
+        document.querySelector('.temperature').innerHTML = `${response.current.temp_c}°C`;
+        document.querySelector('.humidity').innerHTML = `${response.current.humidity}%`;
+        document.querySelector('.wind').innerHTML = `${response.current.wind_kph} km/hr`;
+    } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+    }
 }
+
+        
